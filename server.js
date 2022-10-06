9d956054f11a4983814444b17308ab7d168dfde2
yiladien@hotmail.com <yiladien@hotmail.com>
2022-10-05 23:28:04 -0500

    questions added

--- server.js
+++ server.js
@@ -1,34 +1,12 @@
 const db = require("./db/connection");
 const apiRoutes = require("./routes/apiRoutes");
-const inquirer = require("inquirer");
-const cTable = require("console.table");
-const Game = require("./lib/Game");
+const initializeApp = require("./lib/start");
 
-const express = require("express");
 const mysql = require("mysql2");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Use apiRoutes
-app.use("/api", apiRoutes);
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-// Start server after DB connection
+// DB connection
 db.connect((err) => {
   if (err) throw err;
   console.log("Database connected.");
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  return initializeApp();
 });
-
-new Game().initializeGame();
